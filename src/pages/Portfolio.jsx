a1c698fca759f9b2bef5956173a59fcc50bec03e
IlanVaks <ilanvaks@gmail.com>
2023-06-08 23:57:20 -0400

    fixed-portPage

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -16,6 +16,7 @@ export default function Portfolio () {
         <Row>
           <Col md={7}>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias rerum ratione adipisci praesentium beatae impedit, iure ex vel nisi deleniti, omnis, eaque commodi tenetur corrupti. Error neque suscipit aliquid eveniet.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum possimus similique ut repellat perferendis, sed nesciunt tempora dignissimos delectus quaerat quibusdam non repudiandae aperiam, aliquam earum excepturi labore voluptates rerum.</p>
           </Col>
         </Row>
       </Container>
