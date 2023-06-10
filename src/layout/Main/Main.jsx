6679d270d9da2a72e65548891391cec9416273a3
IlanVaks <ilanvaks@gmail.com>
2023-06-10 16:06:26 -0400

    fixed-main

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -9,6 +9,7 @@ export default function Main () {
         <Row>
           <Col>
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde totam obcaecati blanditiis! Magni, fugit labore? Unde assumenda quae consectetur inventore veritatis, officiis natus, ratione totam sit, nemo eum aut amet!</h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam dicta id dolor ea ullam! Quos vitae maxime nemo eius voluptatum omnis! Fugiat repudiandae repellat facilis quisquam voluptas? Commodi, nobis natus!</p>
           </Col>
         </Row>
       </Container>
