89d82905c51de13bf86eebbce0d39fce0c124504
IlanVaks <ilanvaks@gmail.com>
2023-06-11 21:10:37 -0400

    adding-main

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -10,6 +10,7 @@ export default function Main () {
           <Col>
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde totam obcaecati blanditiis! Magni, fugit labore? Unde assumenda quae consectetur inventore veritatis, officiis natus, ratione totam sit, nemo eum aut amet!</h1>
             <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam dicta id dolor ea ullam! Quos vitae maxime nemo eius voluptatum omnis! Fugiat repudiandae repellat facilis quisquam voluptas? Commodi, nobis natus!</p>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, unde eius asperiores, ducimus vitae illum vero nobis dicta numquam autem voluptas doloribus nisi veritatis? Eum molestiae voluptatibus sed sapiente nihil.</p>
           </Col>
         </Row>
       </Container>
